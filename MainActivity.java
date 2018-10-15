17813e65482f3d50b68b05ba4e0f54513fe76a43
sukanth1998 <sukanth1998@gmail.com>
2018-10-15 20:49:11 +0530

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.sukanth.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.youtube.com/");
+        webView.getSettings().setJavaScriptEnabled(true);
+    }
+}
